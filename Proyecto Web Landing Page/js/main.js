553fa53c414bc468e145dc1d398a894fe35a6b85
Lalo <67574226+OscarQ23@users.noreply.github.com>
2022-06-13 16:50:24 -0600

    Add files via upload

---
+++ Proyecto Web Landing Page/js/main.js
@@ -0,0 +1,9 @@
+
+//inicia la libreria de las animaciones
+AOS.init();
+
+//efecto del menu 
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("abajo",window.scrollY>1);
+})
